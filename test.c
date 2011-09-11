f854b22545ccd5d91f9d603859a9888757121898
Cameron Smith <cwsmith@scorec.rpi.edu>
2011-09-11 01:06:33 -0400

    added echo to test.c

--- test.c
+++ test.c
@@ -3,5 +3,10 @@
 int main(int argc, char** argv)
 {
 	printf("Hello World!\n");
+        if ( argc > 0 ) {
+          for ( int i = 1; i < argc; ++i ) {
+            printf("argv[%d]: %s\n", i, argv[i]);
+          }
+        }
 	return(0);
 }
